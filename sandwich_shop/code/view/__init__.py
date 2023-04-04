005dac1b6aeb7db6f7adafd2288e32f1e7b22c2d
Selin Bengi <sb1213@exeter.ac.uk>
2023-04-04 11:05:25 +0100

    reformatting directory

--- sandwich_shop/code/view/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Package containing all Tkinter GUI modules
-
-Sources for all modules:
-https://www.youtube.com/watch?v=yQSEXcf6s2I&list=PLCC34OHNcOtoC6GglhF3ncJ5rLwQrLGnV&ab_channel=Codemy.com
-https://github.com/flatplanet/Intro-To-TKinter-Youtube-Course
-https://www.geeksforgeeks.org/python-gui-tkinter/
-https://realpython.com/python-gui-tkinter/
-"""
